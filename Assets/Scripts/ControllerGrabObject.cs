be0085f80bd94e96a427f750d5ce800374653136
Paul-Elian <paul.tabarant@gmail.com>
2019-02-07 16:29:31 +0100

    Added project files

---
+++ Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+    // ref. to the object colliding with the controller
+    private GameObject collidingObject;
+    // ref. to the object currently being grabbed
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetCollidingObject(Collider col) {
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+            return;
+        collidingObject = col.gameObject;
+    }
+
+    // Callback methods to handle rigid body's interactions with others
+    public void OnTriggerEnter(Collider other) {
+        SetCollidingObject(other);
+    }
+    private void OnTriggerStay(Collider other) {
+        SetCollidingObject(other);
+    }
+    private void OnTriggerExit(Collider other) {
+        if (!collidingObject)
+            return;
+        collidingObject = null;
+    }
+
+
+    // Handles objects grab/release in the scene
+    private void GrabObject() {
+        objectInHand = collidingObject;
+        collidingObject = null;
+
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+    private FixedJoint AddFixedJoint() {
+
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+    private void ReleaseObject() {
+        if (GetComponent<FixedJoint>()) {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        objectInHand = null;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Controller.GetHairTriggerDown()) {
+            if (collidingObject) {
+                GrabObject();
+            }
+        }
+
+        if (Controller.GetHairTriggerUp()) {
+            if (objectInHand) {
+                ReleaseObject();
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/LineDrawer.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.IO;
+
+public class LineDrawer : MonoBehaviour {
+    // Stores a reference to the controller object data
+    private SteamVR_TrackedObject trackedObj;
+
+    // Line drawing useful attributes
+    private LineRenderer curLine;
+    public Material lineMaterial;
+    public float lineWidth;
+    int pointsIndex = 0;
+
+    // File writing
+    private readonly string filePath = "Assets/Output/data.txt";
+    private StreamWriter writer;
+    private bool newLine = true;
+    private readonly string sep = "; ";
+
+    // Accessing controller input via Controller reference for ease.
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    private void Awake() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void Start() {
+        // Automatically clears data file on startup
+        ClearFileContent();
+    }
+
+    private void Update() {
+        // Trigger pressed for the first time, should create a new line
+        if (Controller.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            AddNewLine();
+        // Trigger held down while already drawing a line, should add a new point
+        } else if (Controller.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+            // Render the line on screen
+            AddVertexToCurLine(trackedObj.transform.position);
+            // Save line data
+            SaveLocalFrame(trackedObj.transform);
+        } else if (Controller.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger)) { 
+            // Close the input stream and add a line jump when the user stopped drawing
+            writer.WriteLine();
+            writer.Close();
+        }
+    }
+
+    // Creates a new game object in the scene, rendering a line drawing
+    // Also initializes file writing for this line
+    private void AddNewLine() {
+        // Add the game object to the scene
+        GameObject lineObj = new GameObject();
+        curLine = lineObj.AddComponent<LineRenderer>();
+        curLine.material = lineMaterial;
+        curLine.startWidth = curLine.endWidth = lineWidth/2;
+        pointsIndex = 0;
+        newLine = true;
+
+        // Open a new input stream to store the line
+        writer = new StreamWriter(filePath, true);
+    }
+
+    // Adds a new vertex to the current line
+    private void AddVertexToCurLine(Vector3 pos) {
+        // Must update the vertices array size each time a new vertex is added
+        curLine.positionCount = pointsIndex + 1;
+        curLine.SetPosition(pointsIndex, pos);
+        pointsIndex++;
+    }
+
+    // Saves the local frame (position, orientation & time) in a file
+    private void SaveLocalFrame(Transform frame) {
+        Vector3 globalPos = frame.position;
+        Quaternion globalRot = frame.rotation;
+
+        string data = Time.time + sep
+                    + globalPos.ToString() + sep
+                    + globalRot.ToString();
+        writer.WriteLine(data);
+    }
+
+    // Clears content of the file associated to the "path" attribute
+    private void ClearFileContent() {
+        StreamWriter writer = File.CreateText(filePath);
+        writer.Flush();
+        writer.Close();
+    }
+}
