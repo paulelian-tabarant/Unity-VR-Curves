93452d6b78e52f5dc8a2390be20ecfc055caf6e2
Paul-Elian <paul.tabarant@gmail.com>
2019-02-12 11:17:23 +0100

    Refactored application for better future use

---
+++ Assets/Scripts/PathDrawer.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+public class PathDrawer : MonoBehaviour {
+    // Stores a reference to the controller object data
+    private SteamVR_TrackedObject trackedObj;
+
+    // Line drawing useful attributes
+    private struct Coords {
+        public Vector3 pos;
+        public Quaternion rot;
+        public float t;
+
+        public Coords(float t, Vector3 pos, Quaternion rot) {
+            this.t = t;
+            this.pos = pos;
+            this.rot = rot;
+        }
+    }
+    private List<Coords>[] pathsArray;
+    private const int MAXSIZE = 100;
+    private int pathIndex = 0;
+
+    // File writing
+    private readonly string filePath = "Assets/Output/data.txt";
+    private readonly string numberFormat = "0.000000";
+    private StreamWriter writer;
+    private readonly string sep = "; ";
+
+    // Accessing controller input via Controller reference for ease.
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    private void Awake() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void Start() {
+        // ... Nothing to do for the moment.
+        pathsArray = new List<Coords>[MAXSIZE];
+    }
+
+    private void Update() {
+        // Trigger pressed for the first time, should create a new line
+        if (Controller.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+            CreateNewPath(trackedObj.transform);
+        // Trigger held down while already drawing a line, should add a new point
+        } else if (Controller.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+            AddPointToCurPath(Time.time, trackedObj.transform);
+        } else if (Controller.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger)) {
+            EndCurPath();
+        }
+    }
+
+    /// <summary>
+    /// Initialize new path handling.
+    /// </summary>
+    /// <param name="coord">The 2-anchor path initial position</param>
+    private void CreateNewPath(Transform coord) {
+        pathsArray[pathIndex] = new List<Coords>();
+        // Clear file content only on the first path drawing to erase previous file content
+        bool append = pathIndex != 0;
+        writer = new StreamWriter(filePath, append);
+    }
+
+    /// <summary>
+    /// Add a new coordinate to current path
+    /// </summary>
+    /// <param name="coord">The {position; rotation} transform to be added</param>
+    private void AddPointToCurPath(float time, Transform coord) {
+        Coords point = new Coords(time, coord.position, coord.rotation);
+        pathsArray[pathIndex].Add(point);
+        SavePoint(writer, point);
+    }
+
+    /// <summary>
+    /// Save a set of coordinates (transform) into a file
+    /// </summary>
+    /// <param name="coord">The Transform object data to be stored</param>
+    private void SavePoint(StreamWriter writer, Coords point) {
+        Vector3 globalPos = point.pos;
+        Quaternion globalRot = point.rot;
+        float time = point.t;
+        string x = globalPos.x.ToString(numberFormat);
+        string y = globalPos.y.ToString(numberFormat);
+        string z = globalPos.z.ToString(numberFormat);
+        string q0 = globalRot.w.ToString(numberFormat);
+        string q1 = globalRot.x.ToString(numberFormat);
+        string q2 = globalRot.y.ToString(numberFormat);
+        string q3 = globalRot.z.ToString(numberFormat);
+
+        string data = time + sep
+                    + '(' + x + ',' + y + ',' + z + ')' + sep
+                    + '(' + q0 + ',' + q1 + ',' + q2 + ',' + q3 + ')';
+        writer.WriteLine(data);
+    }
+
+    /// <summary>
+    /// All procedures required to be done when a path has ended being drawn
+    /// </summary>
+    private void EndCurPath() {
+        writer.WriteLine();
+        writer.Close();
+        if (pathIndex < MAXSIZE)
+            pathIndex++;
+    }
+}
---
+++ Assets/Scripts/PathMeshRenderer.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PathMeshRenderer : MonoBehaviour {
+    [Header("Path display settings")]
+    public Material pathMaterial;
+    public float pathThickness, pathWidth;
+
+    // Path rendering
+    private GameObject pathMesh;
+    MeshFilter meshFilter;
+    MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        pathMesh = new GameObject("Path mesh");
+	}
+
+    // Methods below are from the Road Mesh Creator example.
+/*
+    /// <summary>
+    /// Create a mesh from current stored path. 
+    /// </summary>
+    /// <returns>the corresponding mesh object</returns>
+    private Mesh CreateMeshFromBezierPath(BezierPath bPath) {
+        VertexPath vPath = new VertexPath(bPath);
+        Vector3[] verts = new Vector3[vPath.NumVertices * 8];
+        Vector2[] uvs = new Vector2[verts.Length];
+        Vector3[] normals = new Vector3[verts.Length];
+
+        int numTris = 2 * (vPath.NumVertices - 1);
+        int[] pathTriangles = new int[numTris * 3];
+        int[] underPathTriangles = new int[numTris * 3];
+        int[] sideOfPathTriangles = new int[numTris * 2 * 3];
+
+        int vertIndex = 0;
+        int triIndex = 0;
+        int[] triangleMap = { 0, 8, 1, 1, 8, 9 };
+using PathCreation;
+        int[] sidesTriangleMap = { 4, 6, 14, 12, 4, 14, 5, 15, 7, 13, 15, 5 };
+
+        for (int i = 0; i < vPath.NumVertices; i++) {
+            Vector3 localUp = Vector3.Cross(vPath.tangents[i], vPath.normals[i]);
+            Vector3 localRight = Vector3.Cross(localUp, vPath.tangents[i]);
+
+            Vector3 leftSide = vPath.vertices[i] - localRight * Mathf.Abs(pathWidth);
+            Vector3 rightSide = vPath.vertices[i] + localRight * Mathf.Abs(pathWidth);
+
+            // Top of path vertices
+            verts[vertIndex + 0] = leftSide;
+            verts[vertIndex + 1] = rightSide;
+            // Bottom of path vertices
+            verts[vertIndex + 2] = leftSide - localUp * pathThickness;
+            verts[vertIndex + 3] = rightSide - localUp * pathThickness;
+            // Duplicate vertices to get flat shading for sides of path
+            verts[vertIndex + 4] = verts[vertIndex + 0];
+            verts[vertIndex + 5] = verts[vertIndex + 1];
+            verts[vertIndex + 6] = verts[vertIndex + 2];
+            verts[vertIndex + 7] = verts[vertIndex + 3];
+
+            // uv coordinates on y axis to path normalized length (=times field)
+            uvs[vertIndex + 0] = new Vector2(0, vPath.times[i]);
+            uvs[vertIndex + 1] = new Vector2(1, vPath.times[i]);
+
+            // Top of road normals
+            normals[vertIndex + 0] = localUp;
+            normals[vertIndex + 1] = localUp;
+            // Bottom of road normals
+            normals[vertIndex + 2] = -localUp;
+            normals[vertIndex + 3] = -localUp;
+            // Sides of road normals
+            normals[vertIndex + 4] = -localRight;
+            normals[vertIndex + 5] = localRight;
+            normals[vertIndex + 6] = -localRight;
+            normals[vertIndex + 7] = localRight;
+
+            // Index triangles
+            // condition for the path extremity
+            if (i < vPath.NumVertices - 1) { 
+                for (int j = 0; j < triangleMap.Length; j++) {
+                    pathTriangles[triIndex + j] = vertIndex + triangleMap[j];
+using PathCreation;
+                    underPathTriangles[triIndex + j] = vertIndex + triangleMap[triangleMap.Length - 1 - j] + 2;
+                }
+                for (int j = 0; j < sidesTriangleMap.Length; j++) {
+                    sideOfPathTriangles[triIndex * 2 + j] = vertIndex + sidesTriangleMap[j];
+                }
+            }
+        //pathMeshRenderer = GetComponent<PathMeshRenderer>();
+            vertIndex += 8;
+            triIndex += 6;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = verts;
+        mesh.uv = uvs;
+        mesh.normals = normals;
+        mesh.subMeshCount = 3;
+        mesh.SetTriangles(pathTriangles, 0);
+        mesh.SetTriangles(underPathTriangles, 1);
+        mesh.SetTriangles(sideOfPathTriangles, 2);
+        mesh.RecalculateBounds();
+
+        return mesh;
+    }
+            // Render the line on screen
+
+    // Add MeshRenderer and MeshFilter components to this gameobject if not already attached
+    void AssignMeshComponents() {
+        Transform meshHolder = pathMesh.transform;
+
+        meshHolder.transform.position = Vector3.zero;
+        meshHolder.transform.rotation = Quaternion.identity;
+
+        // Ensure mesh renderer and filter components are assigned
+        if (!meshHolder.gameObject.GetComponent<MeshFilter>()) {
+            meshHolder.gameObject.AddComponent<MeshFilter>();
+        }
+        if (!meshHolder.GetComponent<MeshRenderer>()) {
+            meshHolder.gameObject.AddComponent<MeshRenderer>();
+        }
+
+            // Render the line on screen
+        meshRenderer = meshHolder.GetComponent<MeshRenderer>();
+        meshFilter = meshHolder.GetComponent<MeshFilter>();
+    }
+
+    void AssignMaterials() {
+        if (pathMaterial != null) {
+            meshRenderer.sharedMaterials = new Material[] { pathMaterial };
+        }
+    }
+
+    public void RenderBezierPath(BezierPath path) {
+        AssignMeshComponents();
+        AssignMaterials();
+        meshFilter.mesh = CreateMeshFromBezierPath(path);
+    }*/
+}
